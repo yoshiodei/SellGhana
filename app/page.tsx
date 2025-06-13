20c8b526e999b1888290d47d81c759c91627bbaa
Yoshi Odei <yoshiodei@yahoo.com>
2025-06-13 15:41:30 +0000

    minor style change

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
               ))}
             </div>
           ) : (
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
+            <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
               {products.map((product) => (
                 <ProductCard 
                   key={product.id} 
