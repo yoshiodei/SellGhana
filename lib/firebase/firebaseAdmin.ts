2c7cf6c1545e7940d8d804bbe62e683f5e64cc4e
Yoshi Odei <yoshiodei@yahoo.com>
2025-06-13 13:56:12 +0000

    error fix 001

--- lib/firebase/firebaseAdmin.ts
+++ lib/firebase/firebaseAdmin.ts
@@ -1,16 +1,29 @@
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 
-const app = !admin.apps.length
-  ? admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    })
-  : admin.app();
+// const app = !admin.apps.length
+//   ? admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//         clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+//         privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//       }),
+//     })
+//   : admin.app();
 
-export const adminAuth = app.auth();
-export const adminDB = app.firestore();
+// export const adminAuth = app.auth();
+// export const adminDB = app.firestore();
 
-  
+
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+export default admin;
